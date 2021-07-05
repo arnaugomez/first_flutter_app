9bc95b916016f6257b8263bb983f29bf354eff0e
Arnau Gómez Farell <agomez1f@gmail.com>
2021-07-05 09:53:43 +0200

    Alert and avatar pages

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,12 +9,52 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Alert Page"),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Mostrar alerta"),
+          onPressed: () => _showAlert(context),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.red, shape: StadiumBorder()),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_location),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context, barrierDismissible: true, builder: _renderDialog);
+  }
+
+  Widget _renderDialog(BuildContext context) {
+    return AlertDialog(
+      title: Text("Título"),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text("Este es el contenido de la caja de la alerta"),
+          SizedBox(
+            height: 20,
+          ),
+          FlutterLogo(size: 80)
+        ],
+      ),
+      actions: [
+        TextButton(
+          child: Text("Cancelar"),
+          onPressed: () => Navigator.pop(context),
+        ),
+        TextButton(
+          child: Text("Ok"),
+          onPressed: () => Navigator.pop(context),
+        )
+      ],
     );
   }
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,9 +6,31 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Avatar Page"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Avatar Page"),
+          actions: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://www.tekcrispy.com/wp-content/uploads/2018/10/avatar.png"),
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: CircleAvatar(
+                  child: Text(
+                    "SL",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  backgroundColor: Colors.brown,
+                )),
+          ],
+        ),
+        body: Center(
+          child: FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(
+                "https://www.tekcrispy.com/wp-content/uploads/2018/10/avatar.png"),
+            fadeInDuration: Duration(milliseconds: 200),
+          ),
+        ));
   }
 }
